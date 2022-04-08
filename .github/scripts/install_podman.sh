fa0900aa6154df72e7a0c4cdfd145fb8ab88c3a1
Dan Fornika <dfornika@gmail.com>
2022-04-08 11:05:58 -0700

    suod

--- .github/scripts/install_podman.sh
+++ .github/scripts/install_podman.sh
@@ -15,10 +15,10 @@ DEBIAN_FRONTEND=noninteractive sudo apt-get install -y \
 
 source /etc/os-release
 
-sh -c "echo 'deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list"
-wget -nv https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/xUbuntu_${VERSION_ID}/Release.key -O- | apt-key add -
+sudo sh -c "echo 'deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list"
+sudo wget -nv https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/xUbuntu_${VERSION_ID}/Release.key -O- | apt-key add -
 
-apt-get update -qq -y
-apt-get -qq --yes install podman
+sudo apt-get update -qq -y
+sudo apt-get -qq --yes install podman
 
 podman --version >> artifacts/test_artifact.log
