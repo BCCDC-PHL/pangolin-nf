cac573ab6cb2a2535fa6ebe0742eafdee0a54b88
Dan Fornika <dfornika@gmail.com>
2022-04-08 10:28:38 -0700

    Fix test script

--- .github/scripts/test_output_matches_expected_results.sh
+++ .github/scripts/test_output_matches_expected_results.sh
@@ -5,6 +5,8 @@ set -eo pipefail
 export PATH=/opt/miniconda3/bin:$PATH
 export PATH=/opt/nextflow/bin:$PATH
 
+nextflow -quiet self-update
+
 # write test log as github Action artifact
 echo Nextflow run current PR >> artifacts/test_artifact.log
 NXF_VER=20.10.0 nextflow -quiet run ./main.nf \
@@ -14,7 +16,7 @@ NXF_VER=20.10.0 nextflow -quiet run ./main.nf \
        --outdir test_analysis_results
 
 cp .nextflow.log artifacts/
-cp -r results artifacts/test_analysis_results
+cp -r test_analysis_results artifacts/test_analysis_results
 
 # Compare test results against previously-generated expected results
 echo "Compare ouputs of current PR vs expected results.." >> artifacts/test_artifact.log
